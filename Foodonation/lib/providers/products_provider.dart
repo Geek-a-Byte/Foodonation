e4045a005a9fef51cdf7774065f717077107af25
Nazia Shehnaz <59027621+Geek-a-Byte@users.noreply.github.com>
2020-09-20 16:43:52 +0600

    new images uploaded

--- Foodonation/lib/providers/products_provider.dart
+++ Foodonation/lib/providers/products_provider.dart
@@ -18,8 +18,7 @@ class Products with ChangeNotifier {
         title: 'Package - 01',
         description:
             'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        imageURL:
-            'https://www.goodyearhealth.com/wp-content/uploads/1470895393417.jpeg',
+        imageURL: 'https://imgur.com/fy5oNRr.png',
       ),
     );
     this.addProduct(
@@ -28,8 +27,7 @@ class Products with ChangeNotifier {
         title: 'Package - 02',
         description:
             'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        imageURL:
-            'https://economictimes.indiatimes.com/thumb/msid-74742498,width-1200,height-900,resizemode-4,imgsize-263761/rice-agencies.jpg?from=mdr',
+        imageURL: 'https://imgur.com/JF9fuTx.png',
       ),
     );
     this.addProduct(
@@ -38,8 +36,7 @@ class Products with ChangeNotifier {
         title: 'Package - 03',
         description:
             'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        imageURL:
-            'https://media.istockphoto.com/photos/raw-meat-assortment-beef-lamb-chicken-on-a-wooden-board-picture-id857308908?k=6&m=857308908&s=612x612&w=0&h=fj7W0EChgIrtQb5UVwKVPs7f7PlAM5_GRH21g2YU_X8=',
+        imageURL: 'https://imgur.com/JFqcSqL.png',
       ),
     );
     this.addProduct(
@@ -48,8 +45,7 @@ class Products with ChangeNotifier {
         title: 'Package - 04',
         description:
             'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        imageURL:
-            'https://www.helpguide.org/wp-content/uploads/table-with-grains-vegetables-fruit-768.jpg',
+        imageURL: 'https://imgur.com/e1jFFiR.png',
       ),
     );
     notifyListeners();
