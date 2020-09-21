def72783039399354dafd556e73db386184432f7
Tamim365 <tamim.365.ti@gmail.com>
2020-09-21 12:51:54 +0600

    Check Status Patch Request  Updated

--- Foodonation/lib/providers/product.dart
+++ Foodonation/lib/providers/product.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -16,8 +19,28 @@ class Product with ChangeNotifier {
     this.isChecked = false,
   });
 
-  void toggleFavouriteStatus() {
+  void _setCheckStatus(bool newStatus) {
+    isChecked = newStatus;
+    notifyListeners();
+  }
+
+  void toggleFavouriteStatus() async {
+    final temp = isChecked;
     isChecked = !isChecked;
     notifyListeners();
+    final url = 'https://foodonation-129a8.firebaseio.com/products/$id.json';
+    try {
+      final response = await http.patch(
+        url,
+        body: json.encode({
+          'isChecked': isChecked,
+        }),
+      );
+      if (response.statusCode >= 400) {
+        _setCheckStatus(temp);
+      }
+    } catch (error) {
+      _setCheckStatus(temp);
+    }
   }
 }
